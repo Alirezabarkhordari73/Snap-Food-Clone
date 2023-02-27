af1957e71e7d6efcfb5d90942e3642eef7683359
Alireza Barkhordari <aliirezaa.barkhordarii@gmail.com>
2023-02-27 20:01:26 +0330

    edit pop up

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useLayoutEffect, useState, useEffect } from "react";
-import { useStateContext } from "../Context/StateContext";
 
-import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineClose, AiFillGithub } from "react-icons/ai";
+
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "react-lazy-load-image-component/src/effects/opacity.css";
 
 import Logo from "../Assets/Images/Logo.png";
 import profile from "../Assets/Images/Profile.jpg";
@@ -76,18 +79,26 @@ const Home = () => {
               />
             </div>
             <div className="w-[100%] h-[1px] bg-slate-100" />
-            <div className="w-full flex justify-center mt-8 font-semibold">
+            <div className="w-full flex justify-center mt-7 font-semibold">
               <h1 className="text-[#ff00ae] text-[1.3rem]">
                 به اسنپ فود خوش آمدید
               </h1>
             </div>
             <div className="w-full flex flex-col mt-5">
               <div className="w-full flex justify-center items-center px-5 py-1">
-                <img
-                  src={profile}
-                  className="w-[130px] h-[130px] rounded-full"
-                  alt=""
-                />
+                <div className="w-[100px] h-[100px] object-contain">
+                  <LazyLoadImage
+                    effect="blur"
+                    src={profile}
+                    alt=""
+                    width={"100%"}
+                    style={{
+                      objectFit: "cover",
+                      height: "100%",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </div>
               </div>
               <div className="w-full flex justify-center items-center mt-3">
                 <p>علیرضا برخورداری</p>
@@ -98,11 +109,18 @@ const Home = () => {
               </div>
               <div className="w-full flex justify-center mt-2">
                 <Link
-                  className="text-[#ff00ae] text-[0.88rem]"
+                  className="text-[#ff00ae] text-[0.88rem] flex justify-center items-center gap-2"
+                  target="_blank"
                   to="https://github.com/Alirezabarkhordari73/Snap-Food-Clone"
                 >
+                  <AiFillGithub className="text-[1.4rem]" />
                   سورس پروژه - گیت هاب
                 </Link>
+              </div>
+              <div className="w-full flex justify-center mt-[3.8rem]">
+                <p className="text-[0.6rem] text-[#a7a7a7]">
+                  React - Zustand - React Router - Swiper Js - React Leaflet Map
+                </p>
               </div>
             </div>
           </div>
